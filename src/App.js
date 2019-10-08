3e8d98c584835c0af8986e2dfac42f7b89e09547
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-10-08 11:02:00 +0700

    fully funcional search

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Container, Input, Button, Grid } from "@material-ui/core";
+import Recipe from "./components/Recipe";
 
-function App() {
+const App = () => {
+  
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_API_KEY;
+  const sampleRequest = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  const getRecipes = async () => {
+    const response = await fetch(sampleRequest);
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+    console.log(search)
+  }
+
+  const updateQuery = e => {
+    e.preventDefault()
+    setQuery(search)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={updateQuery}>
+        <Container style={{ textAlign: "center", margin: "20px 0px" }}>
+          <Input
+            style={{ padding: 4 }}
+            placeholder="Search Recipes"
+            id="search"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+          />
+          <Button
+            type="submit"
+            name="search"
+            color="secondary"
+            variant="outlined"
+          >
+            Search
+          </Button>
+        </Container>
+      </form>
+
+      <Container>
+          <Grid 
+            container
+            direction="row"
+            alignItems="center"
+            spacing={3}
+            >
+            {recipes.map(recipe => (
+              <Grid item xs={3} key={recipe.recipe.label}>
+                  <Recipe key={recipe.recipe.label}
+                    title={recipe.recipe.label}
+                    calories={recipe.recipe.calories}
+                    image={recipe.recipe.image}
+                    sourceRecipe={recipe.recipe.source}
+                    ingredients={recipe.recipe.ingredients}
+                    shareAs={recipe.recipe.shareAs}
+                  />
+              </Grid>
+            ))}
+          </Grid>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }),
+);
+
+export default function Recipe(props) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {props.sourceRecipe[0]}
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.title}
+        subheader={`Source : ${props.sourceRecipe}`}
+      />
+      <CardMedia
+        className={classes.media}
+        image={props.image}
+        title={props.title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Calory : {props.calories}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <a href={props.shareAs}>
+        <IconButton aria-label="share">
+            <ShareIcon />
+        </IconButton>
+        </a>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method :</Typography>
+          <Typography paragraph>
+            {props.ingredients.map(ingredient =>(
+              <li key={ingredient.text}>{ingredient.text}</li>
+            ))}
+          </Typography>
+          <Typography>
+            Have a great day
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
